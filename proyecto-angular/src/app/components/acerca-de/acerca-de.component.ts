b54daa43b95c9ac25ff1ffbca297922b551f99a9
ldiazcto <ldiazc@fi.uba.ar>
2022-04-17 15:14:01 -0300

    about me llevado al front

--- proyecto-angular/src/app/components/acerca-de/acerca-de.component.ts
+++ proyecto-angular/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
 
 @Component({
   selector: 'app-acerca-de',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
+  miPortfolio:any;
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    //ahora podemos acceder desde el ng Init, accedemos a los metodos
+    //que defini en service que no deja de ser una clase 
+    this.datosPortfolio.obtenerDatos(1).subscribe(data =>{
+        console.log(data);
+        this.miPortfolio = data;
+        //console.log("Llegue a encabezado");
+        //console.log(data);
+      });
   }
 
 }
