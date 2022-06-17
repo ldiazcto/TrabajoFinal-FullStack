a9313fbef42b9ee43ca3a8b6578b232e7a1e9b18
ldiazcto <ldiazc@fi.uba.ar>
2022-06-17 12:50:55 -0300

    se vuelve a conectar back y fronte por cambio de user a person en la url

--- proyecto-angular/src/app/servicios/portfolio.service.ts
+++ proyecto-angular/src/app/servicios/portfolio.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PortfolioService {
-  url:string = 'http://localhost:8080/user';
+  url:string = 'http://localhost:8080/person';
   constructor(private http:HttpClient) { }
 
   obtenerDatos(id:number):Observable<any>{
