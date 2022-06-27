b89882b0040045ed798a1f0f022a3892919055ca
ldiazcto <ldiazc@fi.uba.ar>
2022-06-26 21:29:30 -0300

    trayendo skill

--- proyecto-angular/src/app/servicios/portfolio.service.ts
+++ proyecto-angular/src/app/servicios/portfolio.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ //este servicio sea inyectado en los componentes, que lo van a consumir
   providedIn: 'root'
 })
 export class PortfolioService {
+  private apiServerUrl = environment.apiBaseUrl;
   url:string = 'http://localhost:8080/person';
   constructor(private http:HttpClient) { }
 
@@ -17,6 +19,23 @@ export class PortfolioService {
     // vamos a enviar un request, y de ahi se obtienelos datos 
     //pero por ahora hacemos que devuelva en un json 
   }
+
+  public getInfo(path: String, info :any): Observable<any>{
+    return this.http.get<any>(`${this.apiServerUrl}/${path}`);
+  }
+  
+  public addInfo(path : String, info : any): Observable<any>{
+    return this.http.post<any>(`${this.apiServerUrl}/${path}/add`, info);
+  }
+
+  public editInfo(path : String, info : any): Observable<any>{
+    return this.http.put<any>(`${this.apiServerUrl}/${path}/edit`, info);
+  }
+
+  public deleteInfo(path : String, Id: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/${path}/delete/${Id}`);
+  }
+
 }
 //los observables no son mas que una coleccion de futuros eventos
 //a los que nos vamos a suscribi 
