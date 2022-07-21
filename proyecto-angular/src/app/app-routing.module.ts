34964806ee1fe9c296e15f7585341ef8aa40be85
ldiazcto <ldiazc@fi.uba.ar>
2022-07-21 01:38:46 -0300

    boton login visual

--- proyecto-angular/src/app/app-routing.module.ts
+++ proyecto-angular/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', component: HomeComponent},
+  // {path: 'home', component: HomeComponent},
+  {path: 'acerca-de', component: AcercaDeComponent},
+  {path: 'proyectos', component: ProyectosComponent},
+  {path: 'skills', component: HabilidadesComponent },
+  {path: 'login', component: IniciarSesionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
